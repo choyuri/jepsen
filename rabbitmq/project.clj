66ce72f60c06620a255bd2a923b58893340c60fb
Cloud User <centos@ip-10-0-236-90.us-west-2.compute.internal>
2017-05-31 18:38:01 +0000

    testing rabbit

--- rabbitmq/project.clj
+++ rabbitmq/project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/aphyr/jepsen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jepsen "0.0.5"]
-                 [com.novemberain/langohr "2.7.1" ]])
+:main jepsen.rabbitmq
+ :dependencies [[org.clojure/clojure "1.8.0"]
+                 [jepsen "0.1.4"]
+	         [com.novemberain/langohr "2.7.1" ] 
+                 [verschlimmbesserung "0.1.3"]])
